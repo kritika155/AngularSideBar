7a736ce1babfa2c9c8353cbb33972422de522374
KRITIKAR <KRITIKAR@birlasoft.com>
2021-04-05 18:38:43 +0530

    sidebar demo

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav-list.component.css']
 })
 export class SidenavListComponent implements OnInit {
-
+  @Output() sidenavClose = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  public onSidenavClose = () => {
+    this.sidenavClose.emit();
+  }
 }
